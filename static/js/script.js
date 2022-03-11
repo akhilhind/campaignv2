81827db85a17e6e9e0b21839f802d4e4f57c0252
akhil <akhil@nagarro.com>
2022-03-11 12:54:00 +0530

    done

---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+import {jsPDF} from '../../node_modules/jspdf';
+
+function saveaspdf(divId, title) {
+    console.log('thanks');
+    var doc = new jsPDF();
+    doc.fromHTML(`<html><head><title>${title}</title></head><body>` + document.getElementById(divId).innerHTML + `</body></html>`);
+    doc.save('insights.pdf');
+}
+
+export default saveaspdf;
